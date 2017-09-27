9d65f0d02101c845684da3a7b9b687496b638c2d
Daria <me.darilla@gmail.com>
2017-09-27 21:59:32 +0200

    fix static routes

--- src/App.js
+++ src/App.js
@@ -2,16 +2,27 @@ import React, { Component } from 'react';
 import SingleQuestionContainer from './components/singleQuestion/SingleQuestionContainer';
 import AllQuestions from './components/allQuestions/AllQuestions';
 import ModalWindowContainer from './components/modalWindow/ModalWindowContainer';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+
+const NoMatch = () => {
+  return (
+    <div>
+      <img src="http://s.quickmeme.com/img/a8/a8022006b463b5ed9be5a62f1bdbac43b4f3dbd5c6b3bb44707fe5f5e26635b0.jpg"/>
+    </div>
+  )
+}
 
 class App extends Component {
   render() {
     return (
       <Router>
         <div>
-          <Route component={AllQuestions} />
-          <Route path={`/single_question`} component={SingleQuestionContainer} />
-          <ModalWindowContainer />
+            <ModalWindowContainer />
+          <Switch>
+            <Route path="/" exact component={AllQuestions} />
+            <Route path="/single_question" component={SingleQuestionContainer} />
+            <Route component={NoMatch}/>
+          </Switch>
         </div>
       </Router>
     );
