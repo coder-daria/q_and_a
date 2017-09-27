fde48fad1392c28cc84dbd71cfdd9dcbc152eef5
Daria <me.darilla@gmail.com>
2017-09-27 09:34:24 +0200

    dynamic data - single question - answer

--- src/components/singleQuestion/questions/Question.js
+++ src/components/singleQuestion/questions/Question.js
@@ -35,7 +35,7 @@ class Question extends React.Component {
           <div className="votes">
             <p className="single_question_unfollow_body">{this.props.user.unfollow}</p>
             <div className="votesCounter">
-              <p><span className="votes_number">{this.props.user.votes}</span> upvotes</p>
+              <p><span className="votes_number">{this.props.user.votes}</span>upvotes</p>
               <div className="arrows">
                 <ArrowUp />
                 <ArrowDown />
