30ef80d7269e9dc2056515a95b39f7a4113635da
Daria <me.darilla@gmail.com>
2017-09-28 16:27:15 +0200

    creating an objec which keeps data concerning answers/replies

--- src/components/singleQuestion/answers/Answer.js
+++ src/components/singleQuestion/answers/Answer.js
@@ -7,23 +7,25 @@ import ArrowDown from 'material-ui-icons/KeyboardArrowDown';
 class Answer extends React.Component {
   renderAnswer(isReply) {
     const space = isReply ? <div className="empty_space single_question_row_empty_space reply" /> : null;
+    const to = this.props.content;
+    console.log(to);
     return (
       <div className="single_question_row main_answer">
         {space}
         <div className="centered_avatar single_question_row_left_item">
-          <Avatar showModal={this.props.showModal} src={this.props.user.avatar_url} props="profile_pic" />
-          <p className="whoCommented"><span className="clickable_user_name" onClick={this.props.showModal}>{this.props.user.name}</span><span className="subTitle">commented it</span><span className="when">{this.props.date.whenCommented}</span></p>
+          <Avatar showModal={this.props.showModal} src={this.props.content.user.avatar} props="profile_pic" />
+          <p className="whoCommented"><span className="clickable_user_name" onClick={this.props.showModal}>{this.props.content.user.name}</span><span className="subTitle">commented it</span><span className="when">{this.props.content.date.whenCommented}</span></p>
         </div>
         <div className="single_question_row_middle_item">
           <div className="answer_header">
-            <p className="whoCommented"><span className="clickable_user_name" onClick={this.props.showModal}>{this.props.user.name}</span><span className="subTitle">commented it</span><span className="when">{this.props.date.whenCommented}</span></p>
+            <p className="whoCommented"><span className="clickable_user_name" onClick={this.props.showModal}>{this.props.content.user.name}</span><span className="subTitle">commented it</span><span className="when">{this.props.content.date.whenCommented}</span></p>
             <p>{this.props.user.answer}</p>
           </div>
         </div>
         <div className="single_question_row_right_item">
           <div className="votes">
             <div className="votesCounter">
-              <p><span className="votes_number">{this.props.user.votes}</span>upvotes</p>
+              <p><span className="votes_number">{this.props.content.answer.votes}</span>upvotes</p>
               <div className="arrows">
                 <ArrowUp />
                 <ArrowDown />
@@ -59,12 +61,8 @@ class Answer extends React.Component {
   }
 }
  Answer.defaultProps = {
-   date: {whenCommented:  "yesterday"},
    user: {
-     name: "Robert",
-     avatar_url: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTT3AnlaspR6f3tPP8qfO1vrdnoDmNul8DL5yVMShsJW5SKtUQEKQ",
      answer:"Numbers or tingling in your feet should be reported to your doctior at your regular visits.", 
-     votes : 8
     }
  };
 
--- src/components/singleQuestion/answers/Answers.js
+++ src/components/singleQuestion/answers/Answers.js
@@ -5,24 +5,63 @@ import '../../common/voting/voting.css';
 
 class Answers extends React.Component {
   render() {
+    const answer = {
+      date: { whenCommented: "yesterday" },
+      answer: {
+        text: "Numbers or tingling in your feet should be reported to your doctior at your regular visits.",
+        votes: 8
+      },
+      user: {
+        name: "Robert",
+        avatar: "https://gl-images.condecdn.net/image/9QP64MA4XvK/crop/200/square"
+      },
+      replies: [
+        {
+          date: { whenCommented: "yesterday" },
+          answer: {
+            text: "Numbers or tingling in your feet should be reported to your doctior at your regular visits.",
+            votes: 8
+          },
+          user: {
+            name: "Tom",
+            avatar: "http://hairandbeautylagos.com/wp-content/uploads/2013/10/classic-cuts-men.jpg"
+          }
+        },
+        {
+          date: { whenCommented: "yesterday" },
+          answer: {
+            text: "Numbers or tingling in your feet should be reported to your doctior at your regular visits.",
+            votes: 12
+          },
+          user: {
+            name: "Orson",
+            avatar: "http://www.goldwell.pl/fileadmin/_processed_/csm_Mens-Reshade_Benefits_With-men-reshade_e10d689fae.jpg"
+          }
+        },
+        {
+          date: { whenCommented: "yesterday" },
+          answer: {
+            text: "Numbers or tingling in your feet should be reported to your doctior at your regular visits.",
+            votes: 1
+          },
+          user: {
+            name: "Julia",
+            avatar: "http://www.behairstyles.com/wp-content/uploads/Medium+Hairstyles+For+50+Year+Old+Woman-628x890.jpg"
+          }
+        }
+      ]
+    }
     return (
       <div className="allAnswers">
         <div className="howManyAnswers"><span className="allAnswers_number">{this.props.answers.howMany}</span>peers already answered</div>
-        <Answer showModal={this.props.showModal}/>
-        <Answer />
-        <Answer />
-        <Answer />
-        <Answer />
-        <Answer />
-        <Answer />
-        <Answer />
-        <Answer />
+        <Answer showModal={this.props.showModal} content={answer}/>
+
       </div>
     );
   }
 }
 Answers.defaultProps = {
-  answers: {howMany: 2}
+  answers: { howMany: 2 }
 };
 
 export default Answers;
