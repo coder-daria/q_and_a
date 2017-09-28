0f2be711ec28de347a49f0549dc9d8b0e6b6a82e
Daria <me.darilla@gmail.com>
2017-09-28 15:37:50 +0200

    return button allows user to come back to the previous page

--- src/components/singleQuestion/SingleQuestion.js
+++ src/components/singleQuestion/SingleQuestion.js
@@ -5,6 +5,7 @@ import Answers from './answers/Answers';
 import ArrowLeft from 'material-ui-icons/KeyboardArrowLeft';
 import Icon from 'material-ui/Icon';
 import Modal from '../modalWindow/ModalWindow';
+import IconButton from 'material-ui/IconButton';
 
 class SingleQuestion extends React.Component {
   renderHeader = () => {
@@ -12,11 +13,13 @@ class SingleQuestion extends React.Component {
       <div className="app_top_section">
         <div className="single_question_header">
           <div className="returnArrow">
+          <IconButton aria-label="Add" onClick={()=>{this.props.history.push('/')}} className="return_button">
             <ArrowLeft />
+          </IconButton>
           </div>
           <div className="add_question">
             <p>Question</p>
-            <Icon>add_circle</Icon>
+              <Icon>add_circle</Icon>
           </div>
           <div className="last_discussed">
             <p>Last time discussed<span className="number">{this.props.timeSinceLastChange.unit}</span>{this.props.timeSinceLastChange.time}</p>
