b9523b1116bffddabf2469abce4a9577b5874696
Daria <me.darilla@gmail.com>
2017-09-28 16:04:00 +0200

    adding component to show 404 error message

--- src/App.js
+++ src/App.js
@@ -3,14 +3,8 @@ import SingleQuestionContainer from './components/singleQuestion/SingleQuestionC
 import AllQuestions from './components/allQuestions/AllQuestions';
 import ModalWindowContainer from './components/modalWindow/ModalWindowContainer';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import PageNotFound from './components/pageNotFound/pageNotFound';
 
-const NoMatch = () => {
-  return (
-    <div>
-      <img src="http://s.quickmeme.com/img/a8/a8022006b463b5ed9be5a62f1bdbac43b4f3dbd5c6b3bb44707fe5f5e26635b0.jpg"/>
-    </div>
-  )
-}
 
 class App extends Component {
   render() {
@@ -21,7 +15,7 @@ class App extends Component {
           <Switch>
             <Route path="/" exact component={AllQuestions} />
             <Route path="/single_question" component={SingleQuestionContainer} />
-            <Route component={NoMatch}/>
+            <Route component={PageNotFound}/>
           </Switch>
         </div>
       </Router>
