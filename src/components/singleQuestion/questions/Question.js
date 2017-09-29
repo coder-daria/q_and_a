1e520a5a4c47975f18bd328dd55a1eee5734ee81
Daria <me.darilla@gmail.com>
2017-09-29 14:41:15 +0200

    minor fixes

--- src/components/singleQuestion/questions/Question.js
+++ src/components/singleQuestion/questions/Question.js
@@ -9,11 +9,17 @@ class Question extends React.Component {
     this.props.showModal();
   }
   renderHeader = () => {
-    const whoIsAsking = <p className="whoIsAsking"><span className="clickable_user_name person_name" onClick={this.toggleModalWindow}>{this.props.user.name}</span><span className="subTitle">is asking:</span></p>;
+    const whoIsAsking = (
+      <div className="whoIsAsking">
+        <span className="clickable_user_name person_name" onClick={this.toggleModalWindow}>
+          {this.props.user.name}
+        </span>
+        <span className="subTitle">is asking:</span>
+      </div>);
     return (
       <div className="single_question_row questionHeader">
         <div className="centered_avatar single_question_row_left_item">
-          <Avatar src={this.props.user.avatar} props="profile_pic" showModal={this.toggleModalWindow}/>
+          <Avatar src={this.props.user.avatar} props="profile_pic" showModal={this.toggleModalWindow} />
           {whoIsAsking}
         </div>
         <div className="single_question_row_middle_item single_question_main_question">
@@ -69,12 +75,12 @@ class Question extends React.Component {
 }
 
 Question.defaultProps = {
-  timeSinceLastChange: {unit: 4, time: "days ago"},
-  user: { 
+  timeSinceLastChange: { unit: 4, time: "days ago" },
+  user: {
     name: "Eva",
     avatar: "http://placecorgi.com/100/100",
-    question: "Will insulin make my patient gain weight?", 
-    questionDescription: "All my patients with diabetes should see on opthalmologist yearly for a dialated beggining at diagnosis in people with type 2 diabetes, and after 5 years in people with type 1 diabetes after puberty.", 
+    question: "Will insulin make my patient gain weight?",
+    questionDescription: "All my patients with diabetes should see on opthalmologist yearly for a dialated beggining at diagnosis in people with type 2 diabetes, and after 5 years in people with type 1 diabetes after puberty.",
     unfollow: "unfollow",
     votes: 19
   }
